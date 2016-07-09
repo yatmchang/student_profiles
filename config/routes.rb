2e6c26dc1488206bdabb97fa9ce0aa9ab6ae55ae
William Stanley <william.t.stanley@gmail.com>
2016-07-09 16:29:55 -0700

    routes refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,6 @@ Rails.application.routes.draw do
 
   get 'password_resets/new'
 
-  resources :projects
-  resources :users, only: [:new, :create, :edit, :update, :show, :index]
-  resources :sessions, only: [:new, :create] do
-    delete :destroy, on: :collection
-  end
-  resources :educations
-  resources :skills
-
   root "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -19,15 +11,16 @@ Rails.application.routes.draw do
   post "/contact/:user_id" => "profiles#contact", as: :contact
 
   resources :users, only: [:new, :create, :edit, :update, :show, :index] do
-    resources :profiles, only: [:edit, :update] do
+    resources :profiles, only: [:edit, :update], as: :profile  do
       resources :links, only: [:create, :update, :destroy]
       resources :educations, only: [:create, :update, :destroy]
       resources :skills, only: [:create, :update, :destroy]
+      resources :projects, only: [:create, :update, :destroy]
+      resources :experiences, only: [:create, :update, :destroy]
     end
   end
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
-  resources :experiences
   resources :password_resets
 end
