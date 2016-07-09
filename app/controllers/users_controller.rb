1cf7a2864a74ab15c8b5b992f26a8a556aefc79c
Kenan Philips <philipskenan@gmail.com>
2016-07-09 12:07:02 -0700

    added password update and cancancan gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+  class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:new, :create, :index]
 
   def new
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
 
   has_one :profile, dependent: :destroy
 
+  attr_accessor :current_password
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format:  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validate :current_password_is_correct, on: :update
 
   after_create :make_profile
 
@@ -13,10 +16,19 @@ class User < ActiveRecord::Base
     "#{first_name} #{last_name}".titleize
   end
 
-
   private
 
   def make_profile
-    Profile.create(user: Uder.last)
+    Profile.create(user: User.last)
   end
+
+  def current_password_is_correct
+    if !password.blank?
+      user = User.find_by_id(id)
+      if (user.authenticate(current_password) == false)
+        errors.add(:current_password, "is incorrect.")
+      end
+    end
+  end
+
 end
