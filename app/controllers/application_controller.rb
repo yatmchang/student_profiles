a552fac20088c25546f9f5c8df5a9001af937ecf
Kenan Philips <philipskenan@gmail.com>
2016-07-08 19:07:26 -0700

    initialize users model and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def authenticate_user!
+    redirect_to new_session_path, alert: "Please sign in." unless session[:user_id].present?
+  end
+
 end
