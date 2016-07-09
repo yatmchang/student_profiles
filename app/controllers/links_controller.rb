3a07bdf98ae241c13d7bf5bedb77eb1b882cf3be
William Stanley <william.t.stanley@gmail.com>
2016-07-09 00:20:36 -0700

    Add profile model and Links model and controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,35 @@
+class LinksController < ApplicationController
+  before_action :find_link, only: [:update, :destroy]
+
+  def create
+    link = Link.new(link_params)
+    user = current_user
+    link.user = user
+    if link.save
+      redirect_to user_path(current_user)
+    else
+      render :root
+    end
+  end
+
+  def update
+    if @link.update(link_params)
+      redirect_to user_path(current_user)
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to user_path(current_user)
+  end
+
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :href)
+  end
+
+  def find_link
+    @link = Link.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  has_one :profile, dependent: :destroy
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format:  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
+  after_create :make_profile
+
   def full_name
     "#{first_name} #{last_name}".titleize
+  end
+
+
+  private
+
+  def make_profile
+    Profile.create(user: Uder.last)
   end
 
 end
