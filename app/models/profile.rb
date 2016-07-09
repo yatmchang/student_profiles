00ff98a2edb3fc128c6a507e578ba63195d52525
Wei Yi <weiyi2017@gmail.com>
2016-07-09 14:15:28 -0700

    add relation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,9 @@
 class Profile < ActiveRecord::Base
+  belongs_to :profile
   belongs_to :user
   has_many :links
   has_many :experiences, dependent: :destroy
   has_many :educations
   has_many :skills
-  
+
 end
