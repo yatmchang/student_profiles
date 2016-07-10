e1f16eef7025894c909183d3f995af7d79aa592c
Tong <tong.wei@mail.mcgill.ca>
2016-07-10 16:32:37 -0700

    add progress bar

---
+++ app/assets/javascripts/progress.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $("#skill-tab").click(function(){
+    var profile = $('#skill-tab').data("profileid")
+    var user = $("#skill-tab").data("userid")
+    $.ajax({
+      method: "GET",
+      url: "/users/" + user + "/profiles/" + profile + "/skills.json",
+      success: function(data){
+        console.log(data)
+        for (var i = 0; i < data.length; i++) {
+          progBar(data[i].rating, data[i].id)
+        }
+
+      },
+      error: function(){
+        alert('no!');
+      }
+    })
+  })
+
+  // @param  {Number} percent
+  // @param
+
+  function progBar(percent, id) {
+    var width = $("ul.nav-tabs").width();
+	  var progressBarWidth = percent * width / 10;
+    $("#skill_" + id).animate(
+      {width: progressBarWidth }, 500
+    ).html();
+  }
+})
+
+
+//percent + "%&nbsp;"
