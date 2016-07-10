67f73e32fe7aa8e30401ee1e8f1e12b89da12ebf
Yat Chang <yatmchang@gmail.com>
2016-07-09 20:14:41 -0700

    fixed conflict between forgot and reset password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
   class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :create, :index]
+  before_action :authenticate_user!, except: [:new, :show, :create, :index]
 
   def new
     @user = User.new
@@ -24,7 +24,7 @@
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
