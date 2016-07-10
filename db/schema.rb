ad0c45eb584f407a247b9139e0206e3a98c7a43f
Paul Lam <paulhklam1122@gmail.com>
2016-07-10 12:17:30 -0700

    third commit

--- db/schema.rb
+++ db/schema.rb
@@ -112,17 +112,11 @@ ActiveRecord::Schema.define(version: 20160710032104) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-<<<<<<< e27bdb4a9e538c5780933c1c8e523f14cfe508f0
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
-=======
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.boolean  "is_admin",        default: false
->>>>>>> interim commit
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
