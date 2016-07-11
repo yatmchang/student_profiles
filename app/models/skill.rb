fead8c8a5c6061c1c85a187ef94da8371c8022ed
Wei Yi <weiyi2017@gmail.com>
2016-07-11 08:43:55 -0700

    asd

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,6 @@
 class Skill < ActiveRecord::Base
   belongs_to :profile
+  validates :title, presence: true, uniqueness: true
+  validates :rating, presence: true, numericality: {less_than_or_equal_to: 10}
+
 end
