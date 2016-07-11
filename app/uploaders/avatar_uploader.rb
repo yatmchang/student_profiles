ed8804f8c2c545513c03e74e33c848baa82b20fa
William Stanley <william.t.stanley@gmail.com>
2016-07-11 07:47:20 -0700

    make online pictures work

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,7 +9,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
 
   #storage :file
-  storage :fog
+   storage :fog
 
 
   # Override the directory where uploaded files will be stored.
